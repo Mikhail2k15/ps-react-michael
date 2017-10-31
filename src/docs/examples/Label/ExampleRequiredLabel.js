b3331eddc4e15b4e5798e3ca5d8612a02be0871e
Mikhail Bondarevsky <mibon@microsoft.com>
2017-10-31 15:34:02 +0300

    finished Atoms module :)

---
+++ src/docs/examples/Label/ExampleRequiredLabel.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Label from 'ps-react/Label';
+
+export default function ExamplRequired(){
+    return <Label htmlFor="test" required label="test label optional"/>;
+}
